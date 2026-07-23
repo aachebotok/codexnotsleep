0d164d030c6504838d05597e47a605d93d193ec6
Наер Михаил Александрович <mikhail.naer@gmail.com>
2026-07-23 13:59:06 +0300

    Fit menu window to content

--- Sources/MethamphetamineApp/MethamphetamineApp.swift
+++ Sources/MethamphetamineApp/MethamphetamineApp.swift
@@ -32,5 +32,6 @@ struct MethamphetamineApplication: App {
         .accessibilityLabel("Methamphetamine: \(controller.statusTitle)")
     }
     .menuBarExtraStyle(.window)
+    .windowResizability(.contentSize)
   }
 }
